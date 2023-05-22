0ab25015c8789a756c369a22a12b0117dca35527
reza <reza.pratama@concretejakarta.com>
2023-05-22 17:57:44 +0700

    revisi tampilan lagi

--- src/pages/Amplop.js
+++ src/pages/Amplop.js
@@ -6,53 +6,52 @@ import ButtonGradient from "../componenets/ButtonGradient";
 import { DaunBottomLeft, DaunBottomRight, DaunTopLeft, DauntTopRight } from "../componenets/Daun";
 
 const Amplop = () => {
-    const [showPopup, setShowPopup] = useState(true)
+    const [showAmplop, setShowAmplop] = useState(true)
 
     useEffect(() => {
-        if (showPopup) {
+        if (showAmplop) {
             document.body.style.overflow = "hidden"
         }
     }, []);
 
     const buttonHandler = () => {
         const amplop = document.querySelector('.customSwipeAmplop');
+        setShowAmplop(false)
         amplop.style.transform = 'translateY(-100%)';
         document.body.style.overflow = "scroll"
-        setShowPopup(false)
     }
 
     return (
         <>
-            {
-                showPopup && (
-                    <div className="fixed z-[9999] transition duration-500 ease-in-out delay-75 customSwipeAmplop w-full h-full mx-auto bg-slate-50 flex flex-col justify-center items-center !overflow-hidden">
-                        <DaunTopLeft />
-                        <DauntTopRight />
-                        <DaunBottomLeft />
-                        <DaunBottomRight />
+            {/* {
+                showAmplop && ( */}
+            <div className="fixed z-[9999] transition duration-500 ease-in-out delay-75 customSwipeAmplop w-screen h-full mx-auto bg-slate-50 flex flex-col justify-center items-center overflow-hidden">
+                <DaunTopLeft />
+                <DauntTopRight />
+                <DaunBottomLeft />
+                <DaunBottomRight />
 
-                        <div className="border-2 border-black rounded-full">
-                            <div className="rounded-full border-2 overflow-hidden border-transparent object-cover">
-                                <img
-                                    src={ring}
-                                    className="img w-[130px] h-[130px] object-cover"
-                                />
-                            </div>
-                        </div>
-
-                        <div className="text-4xl">
-                            Him & Her
-                        </div>
-
-                        <div className="text-center personal-name-style text-3xl p-5 font-normal">
-                            Wedding Apps
-                        </div>
-
-                        <ButtonGradient onClick={buttonHandler} />
+                <div className="border-2 border-black rounded-full">
+                    <div className="rounded-full border-2 overflow-hidden border-transparent object-cover">
+                        <img
+                            src={ring}
+                            className="img w-[170px] h-[170px] object-cover"
+                        />
                     </div>
-                )
-            }
+                </div>
 
+                <div className="text-4xl">
+                    Him & Her
+                </div>
+
+                <div className="text-center personal-name-style text-3xl p-5 font-normal">
+                    Wedding Apps
+                </div>
+
+                <ButtonGradient onClick={buttonHandler} />
+            </div>
+            {/* //     )
+            // } */}
         </>
     );
 };
--- src/pages/MainPages.js
+++ src/pages/MainPages.js
@@ -15,7 +15,7 @@ const MainPages = () => {
             <Amplop />
 
             <section className="flex justify-center relative w-full h-screen">
-                <div className="bg-slate-50 w-full h-full relative overflow-hidden">
+                <div className="bg-slate-50 w-full h-screen relative overflow-hidden">
                     <div className="!absolute w-[10rem] h-[10rem] top-0 left-0 z-50">
                         <DaunTopLeft />
                     </div>
@@ -29,15 +29,15 @@ const MainPages = () => {
                         <DaunTopLeft />
                     </div>
 
-                    <div className="mb-5 relative">
-                        <div className="absolute top-0 left-0 w-full h-24 bg-gradient-to-b from-slate-50 to-transparent z-10">&nbsp;</div>
-                        <img src={imgPersonalia} className="w-full h-screen opacity-50 object-cover" />
-                        <div className="absolute bottom-0 left-0 w-full h-24 bg-gradient-to-t from-slate-50 to-transparent">&nbsp;</div>
+                    <div className="mb-5">
+                        {/* <div className="absolute top-0 left-0 h-24 bg-gradient-to-b w-full from-slate-50 to-transparent z-30">&nbsp;</div> */}
+                        <img src={imgPersonalia} className="w-full h-screen opacity-20 object-cover contrast-200" />
+                        <div className="absolute bottom-0 left-0 w-full h-24 bg-gradient-to-t from-white to-transparent">&nbsp;</div>
 
                         <div className="absolute top-0 left-0 flex flex-col justify-center items-center w-full h-full gap-5">
                             <p className="text-3xl">Him & Her</p>
                             <p className="text-4xl">Sabtu, 30 Mei 2023</p>
-                            {/* <CountDown /> */}
+                            <CountDown />
                         </div>
                     </div>
                 </div>
