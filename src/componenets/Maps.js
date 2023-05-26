3701cb15944db22ec73252958020ec3ab5915dac
reza <reza.pratama@concretejakarta.com>
2023-05-26 18:25:24 +0700

    maps on going to fixed

--- src/componenets/Maps.js
+++ src/componenets/Maps.js
@@ -2,7 +2,7 @@ import React from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 const Maps = () => {
-    return <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} className="w-[30rem] h-[30rem] bg-red-300 overflow-hidden">
+    return <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} className="w-[20rem] h-[20rem] lg:w-[30rem] lg:h-[30rem] bg-red-300 overflow-hidden">
         <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
--- src/pages/MainPages.js
+++ src/pages/MainPages.js
@@ -111,7 +111,7 @@ const MainPages = () => {
                                             <br />
                                             menjadi bagian dari hari istimewa kami!
                                         </p>
-                                        {/* <CountDown /> */}
+                                        <CountDown />
                                         <p className="uppercase text-xl lg:text-4xl">Sabtu, 30 Mei 2023</p>
                                     </div>
                                 </div>
